51e8ef5603c41a1535d8be2e244b2b4a65658849
董秋生 <2221071726@qq.com>
2020-09-17 22:45:21 +0800

    完成用户列表功能开发

--- src/main.js
+++ src/main.js
@@ -5,11 +5,16 @@ import './plugins/element.js'
 import './assets/css/global.css'
 import axios from 'axios'
 
-Vue.prototype.$http = axios
-
 Vue.config.productionTip = false
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 最后必须return
+  return config
+})
+Vue.prototype.$http = axios
 
 new Vue({
   router,
