3719b7ae270bb0709c78269b50b805c8751fd46a
董秋生 <2221071726@qq.com>
2020-09-29 19:58:02 +0800

    完成了分类功能

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import router from './router'
 import './plugins/element.js'
 import './assets/css/global.css'
 import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
 
 Vue.config.productionTip = false
+Vue.component('tree-table', TreeTable)
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
